5d5fd91fe9fc2d40fe95481ebf391d00d8f921ca
wutc <210114@gs.hs.ntnu.edu.tw>
2024-07-29 14:58:25 +0800

    add hidden function

--- index.js
+++ index.js
@@ -14,35 +14,43 @@ function verify(){
     //section 2
     two = document.getElementById('2');
     let specialChars = /[!@#$%^&*(),.?":{}|<>]/;
+    if(CurrentCount==1){
     if(specialChars.test(valueofinputbox)){
         //yes
         two.classList.add('greencolor');
         two.classList.remove('redcolor');
+        CurrentCount++;
     }else{
         two.classList.add('redcolor');
         two.classList.remove('greencolor');
-    }
+    }}
     //section 3
     three = document.getElementById('3');
-    if(/\d/.test(valueofinputbox)){
+    
+    if(CurrentCount==2){
+    if(/\d/.test(valueofinputbox)&&CurrentCount==2){
         //fit req
         three.classList.add('greencolor');
         three.classList.remove('redcolor');
+        CurrentCount++;
     }else{
         three.classList.remove('greencolor');
         three.classList.add('redcolor');
-    }
+    }}
     //section 4
     four = document.getElementById('4');
     let BigChars = /[ABCDEFGHIJKLMNOPQRSTUVWYXZ]/;
-    if(BigChars.test(valueofinputbox)){
+    
+    if(CurrentCount==3){
+    if(BigChars.test(valueofinputbox)&&CurrentCount==3){
         //fit req
         four.classList.add('greencolor');
         four.classList.remove('redcolor');
+        CurrentCount++;
     }else{
         four.classList.remove('greencolor');
         four.classList.add('redcolor');
-    }
+    }}
     
     
 }
