809fddbca03073692337117454e8ac44db01d4b0
Justin <chenhao.lin0121@gmail.com>
2024-07-29 16:02:03 +0800

    add section 6 and 7 7 fail

--- index.js
+++ index.js
@@ -1,49 +1,84 @@
 CurrentCount = 1;
-function verify(){
+function verify() {
     t = document.getElementById('thebox');
     valueofinputbox = document.getElementById('thebox').value;
     one = document.getElementById('1');
-    if(valueofinputbox.length>0){
+    if (valueofinputbox.length > 0) {
         //yes
         one.classList.add('greencolor');
         one.classList.remove('redcolor');
-    }else{
+    } else {
         one.classList.add('redcolor');
         one.classList.remove('greencolor');
     }
     //section 2
     two = document.getElementById('2');
     let specialChars = /[!@#$%^&*(),.?":{}|<>]/;
-    if(specialChars.test(valueofinputbox)){
+    if (specialChars.test(valueofinputbox)) {
         //yes
         two.classList.add('greencolor');
         two.classList.remove('redcolor');
-    }else{
+    } else {
         two.classList.add('redcolor');
         two.classList.remove('greencolor');
     }
     //section 3
     three = document.getElementById('3');
-    if(/\d/.test(valueofinputbox)){
+    if (/\d/.test(valueofinputbox)) {
         //fit req
         three.classList.add('greencolor');
         three.classList.remove('redcolor');
-    }else{
+    } else {
         three.classList.remove('greencolor');
         three.classList.add('redcolor');
     }
     //section 4
     four = document.getElementById('4');
     let BigChars = /[ABCDEFGHIJKLMNOPQRSTUVWYXZ]/;
-    if(BigChars.test(valueofinputbox)){
+    if (BigChars.test(valueofinputbox)) {
         //fit req
         four.classList.add('greencolor');
         four.classList.remove('redcolor');
-    }else{
+    } else {
         four.classList.remove('greencolor');
         four.classList.add('redcolor');
     }
-    
-    
+    //section 5
+    five = document.getElementById('5');
+    if (valueofinputbox.includes("cnmc")) {
+        //fit req
+        five.classList.add('greencolor');
+        five.classList.remove('redcolor');
+    } else {
+        five.classList.remove('greencolor');
+        five.classList.add('redcolor');
+    }
+    //section 6
+    six = document.getElementById('6');
+    if (valueofinputbox.includes("west") || valueofinputbox.includes("West")) {
+        //fit req
+        six.classList.add('greencolor');
+        six.classList.remove('redcolor');
+    } else {
+        six.classList.remove('greencolor');
+        six.classList.add('redcolor');
+    }
+    //section 7
+    seven = document.getElementById('7');
+    eeee = valueofinputbox.match(/[0-9]+\d/g);
+    num = 0;
+    if (eeee != null) {
+        for (i = 0; i < eeee.length; i++) {
+            num += eeee[i];
+        }
+    }
+    if (num == 1947) {
+        //fit req
+        seven.classList.add('greencolor');
+        seven.classList.remove('redcolor');
+    } else {
+        seven.classList.remove('greencolor');
+        seven.classList.add('redcolor');
+    }
 }
 valueofinputbox = document.getElementById('thebox').value;
--- rule.md
+++ rule.md
@@ -2,9 +2,9 @@ the string must contain at least one char
 the string must contain one upper case char
 the string must contain a number
 the string must contain a symbol
-the string must contain "hsnu cnmc"
+the string must contain "cnmc"
 the string must contain the azimuth of the given building
-the string must contain the year when hsnu established year of hsnu
+the sum of numbers in string must be the established year of hsnu
 the string must contain the last class number in your year
 the string must contain the first name of our principle
 the string must contain the serial number of our club
