4735a78c25ad0d6a1a5c36d97af514d818c076c8
wutc <210114@gs.hs.ntnu.edu.tw>
2024-07-29 14:36:30 +0800

    add basic level and flex items

---
+++ index.js
@@ -0,0 +1,50 @@
+function verify(){
+    t = document.getElementById('thebox');
+    valueofinputbox = document.getElementById('thebox').value;
+    // var reg = new RegExp(' /\d/');
+    //section 1
+    one = document.getElementById('1');
+    if(valueofinputbox.length>0){
+        //yes
+        one.classList.add('greencolor');
+        one.classList.remove('redcolor');
+    }else{
+        one.classList.add('redcolor');
+        one.classList.remove('greencolor');
+    }
+    //section 2
+    two = document.getElementById('2');
+    let specialChars = /[!@#$%^&*(),.?":{}|<>]/;
+    if(specialChars.test(valueofinputbox)){
+        //yes
+        two.classList.add('greencolor');
+        two.classList.remove('redcolor');
+    }else{
+        two.classList.add('redcolor');
+        two.classList.remove('greencolor');
+    }
+    //section 3
+    three = document.getElementById('3');
+    if(/\d/.test(valueofinputbox)){
+        //fit req
+        three.classList.add('greencolor');
+        three.classList.remove('redcolor');
+    }else{
+        three.classList.remove('greencolor');
+        three.classList.add('redcolor');
+    }
+    //section 4
+    four = document.getElementById('4');
+    let BigChars = /[ABCDEFGHIJKLMNOPQRSTUVWYXZ]/;
+    if(BigChars.test(valueofinputbox)){
+        //fit req
+        four.classList.add('greencolor');
+        four.classList.remove('redcolor');
+    }else{
+        four.classList.remove('greencolor');
+        four.classList.add('redcolor');
+    }
+    
+    
+}
+valueofinputbox = document.getElementById('thebox').value;
--- rule.md
+++ rule.md
@@ -2,4 +2,10 @@ the string must contain at least one char
 the string must contain one upper case char
 the string must contain a number
 the string must contain a symbol
-the string must contain a lower char
+the string must contain "hsnu cnmc"
+
+
+
+
+
+the string must contain "B02"
